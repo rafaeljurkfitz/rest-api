731ee2f676adb887c5aaadfbf8b1c69b3f675905
Rafael Jürkfitz <rafaeljurkfitz@hotmail.com>
2022-04-22 19:37:02 -0300

    update seeders files configuration

--- src/seeders/20220419205549-users.js
+++ src/seeders/20220419205549-users.js
@@ -6,19 +6,19 @@ module.exports = {
         await queryInterface.bulkInsert('users', [
         {
             login: 'Jose_Silva45',
-            password: '@jgl52846',
+            password: 'sha1$053edb6d$1$f29d1dcd0b7d44ed9c17a951a1c58206d047d322',
             createdAt: new Date(),
             updatedAt: new Date(),
         },
         {
             login: 'MariaEduarda852',
-            password: '2584563214',
+            password: 'sha1$e97a1224$1$28714acc1e20ca3e712b3820248ddd612f86c048',
             createdAt: new Date(),
             updatedAt: new Date(),
         },
         {
             login: 'GuilhermeRocha52',
-            password: 'umapontenomeiodocaminho',
+            password: 'sha1$f5e30964$1$81b3abc5342d338ec9fcdc8440716a70583f8257',
             createdAt: new Date(),
             updatedAt: new Date(),
         }
